0a9a026c8e0a9f4c3d915ccd153fd869cede9894
Pubuditha2001 <pubuditha2001@gmail.com>
2025-06-27 00:10:30 +0530

    profile design and tabs

---
+++ lib/presentation/screens/profile/tabs/description_posts_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPostsTab extends StatelessWidget {
+  const DescriptionPostsTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Description-based posts will appear here.',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
