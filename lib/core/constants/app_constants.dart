f9f514844e64efe2969596c25a9ca7433c771e82
desynkd <ravindupeeris180@gmail.com>
2025-07-12 17:11:43 +0530

    Change token manually in app constraint

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,5 +7,5 @@ class AppConstants {
 
   // Spotify Access Token
   static const String spotifyAccessToken =
-      'BQDzF9uuuq8kGRO0Hh4Rz_hfMZC4MzMtDD8A7_0eQonBQTUw6fKfQ64aXYE9OuwXWExkgZAXztZ1gDALm06t4wsn_yGUpI_v-lQLw5vvDu5Ni7mwRaDlwbwafsYZOmf2sNfliRxN8JBEN7stW9Yhv6Nqf2Vdz4kBvTYU2JP3UFaUdcyCSsejQEm8D-dHeMW_iGwNf9b-ebO4g7y_RlTyNcdslkNVK9hsJLxDrjCsmwSiEe32a3zonYtmKx5ha7t8STcIbER_';
+      'BQDX_5v6Abn64-KGwh8fEiusZNn4aLtduFPjIYqz9vUlvdUjoQ4HNZ49L4PFF6GVroOrnC3pzCypYkJFhLEBBxfvtl-hZX96HB0A3kRk62c0MU7Wjg-UErFqobb1tH14t_TVcaU58iqGsKzUxFlEoW4NkMJZhB3T1ODSw8lDOvRv_dEIKWr-axOng1NAWDsUEnI7NGMcLAyIoAN-4pBBMIDjNeCOvgga8d8jd0mnNMf5Z5tQYeYQ2jfKLE26xyY_7w';
 }
